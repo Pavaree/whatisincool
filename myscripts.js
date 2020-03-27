367cca9bc2d9c0dfe63f5645ca14aa5a8e4f29bb
cracker <41380234+Pavaree@users.noreply.github.com>
2020-03-27 17:46:06 +0700

    Add files via upload
    ✏️

---
+++ myscripts.js
@@ -0,0 +1,3 @@
+function mockupClick(pageToHide,pageToShow){ document.querySelector('#'+pageToHide).style.display = "none";         
+document.querySelector('#'+pageToShow).style.display = "block";
+}
